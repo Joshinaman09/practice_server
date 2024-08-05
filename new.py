0b6c530e283c201423a49dd1939e5ac24078dceb
Naman Joshi <Joshinaman09@gmail.com>
2024-08-05 14:29:58 +0530

    Iniral Code Added

--- new.py
+++ new.py
@@ -1,24 +1,10 @@
-class Dog:
+a = 20
+b = 30
+c=a*b
+print(c)
 
-    # class attribute
-    attr1 = "mammal"
-
-    # Instance attribute
-    def __init__(self, name):
-        self.name = name
-
-# Driver code
-# Object instantiation
-Rodger = Dog("Rodger")
-Tommy = Dog("Tommy")
-
-# Accessing class attributes
-print("Rodger is a {}".format(Rodger.__class__.attr1))
-print("Tommy is also a {}".format(Tommy.__class__.attr1))
-
-# Accessing instance attributes
-print("My name is {}".format(Rodger.name))
-print("My name is {}".format(Tommy.name))
-
-
-print("new feature added in the branch")
+def new(a,b):
+    if a > b:
+        print("Hello")
+    else:
+        print('nothing')
